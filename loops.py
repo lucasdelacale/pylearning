a8ac62e63256fe9783531d13feaa7f3335417ca9
lucasdelacale <corp.lucasmartins@gmail.com>
2025-03-15 18:16:26 -0300

    update

--- loops.py
+++ loops.py
@@ -3,5 +3,20 @@ Repetições
 while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 loop infinito: quando o código não tem fim
-
+while True: #loop e condição
 '''
+#Criando um contador para explicar o loop while
+
+
+copo = 0 #Um copo vázio. 
+capacidade = 300
+
+while copo < capacidade : #Capacidade do copo
+#enquanto for menor que 10 a condição é verdadeira
+#e o loop continua sendo executado
+    copo += 50 
+    #Manipula a quantidade em que o loop é contado
+    print(f'Enchendo {copo} de {capacidade} ml') 
+    #printa o resultado
+    ...
+
