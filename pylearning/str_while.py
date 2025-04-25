384abd7eb8e0dc9d12f328159dd2a9392fbafadd
lucasdelacale <corp.lucasmartins@gmail.com>
2025-04-25 00:14:38 -0300

    Exercício while, arquivo atr_while

---
+++ pylearning/str_while.py
@@ -0,0 +1,12 @@
+'''
+Iterando strings com while
+
+'''
+#Indices----0123456789101112
+nome = 'Lucas Martins'
+indice = 0
+
+while indice <= len(nome):
+    print(nome[indice])
+    indice += 1
+    
